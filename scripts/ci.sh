bd24d823fcd957bcfe08a5074bf789239c9d9e11
Mike Farah <mike.farah@orchestrated.io>
2018-01-09 10:30:17 +1100

    use name field

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -14,6 +14,7 @@ function report_failed {
     -d "{
     \"itemUri\": \"-L0rXVgD5kjkRUZS4Ta1/-L0rXVpIREppHGSJqQcY/cicd/$itemId\",
     \"itemContents\": {
+      \"name\": \"$CIRCLE_PROJECT_REPONAME ($CIRCLE_BRANCH)\",
       \"url\": \"$url\",
       \"description\": \"$description\",
       \"failed\": $1
@@ -23,7 +24,6 @@ function report_failed {
 
 trap "report_failed true" ERR
 
-fail
 ./scripts/devtools.sh
 make local test
 
