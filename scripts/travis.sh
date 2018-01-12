e0984bc8ac1c9c5dd8c27a46357d26ce4375d038
Mike Farah <mike.farah@orchestrated.io>
2018-01-12 17:08:03 +1100

    added travis config

---
+++ scripts/travis.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+# For security, we recommend extracting the api key out as an environment variable in your CI
+# https://docs.travis-ci.com/user/environment-variables/#Defining-encrypted-variables-in-.travis.yml
+apiKey=$DASHTIC_API
+
+name="" # optionally set a name
+itemId="$TRAVIS_REPO_SLUG-$TRAVIS_BRANCH"
+url="https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_NUMBER"
+description="$TRAVIS_COMMIT_MESSAGE"
+
+function report_failed {
+  curl \
+    -H "X-Api-Key: $apiKey" \
+    -H "Content-Type: application/json" \
+    -d "{
+    \"itemUri\": \"-L0rXVgD5kjkRUZS4Ta1/-L2cvVcWYA4CNmwL7F3o/test/$itemId\",
+    \"itemContents\": {
+      \"name\": \"$name\",
+      \"url\": \"$url\",
+      \"description\": \"$description\",
+      \"failed\": $1
+    }
+  }" https://daas-staging.firebaseapp.com/api/update-item
+}
+
+trap "report_failed true" ERR
+
+./scripts/devtools.sh
+make local test
+
+report_failed false
