aad3201ebb77d51de4d5ed99416b583e756a39c9
Mike Farah <mike.farah@orchestrated.io>
2018-01-16 14:33:43 +1100

    fail go build

--- scripts/go.sh
+++ scripts/go.sh
@@ -3,28 +3,34 @@ set -e
 
 # For security, we recommend extracting the api key out as an environment variable in your CI
 # https://docs.gocd.org/current/faq/environment_variables.html
-apiKey="abc123-abc123-abc123"
+apiKey="$DASHTIC_API_KEY"
 
-itemId="$GO_PIPELINE_NAME-$GO_JOB_NAME"
+repo="$GO_PIPELINE_NAME-$GO_JOB_NAME"
+
+name="$repo" # optionally set a name
+itemId="$repo"
 url=""
 description="$GO_STAGE_NAME"
 
 function report_failed {
-  curl -v \
+  curl \
     -H "X-Api-Key: $apiKey" \
     -H "Content-Type: application/json" \
     -d "{
-    \"itemUri\": \"accountId/dashboardId/boxId/$itemId\",
+    \"itemUri\": \"-L2cyF1pYaFXX43o3c-B/-L2cyF0kR4SYk7iFu4ii/cicd/$itemId\",
     \"itemContents\": {
+      \"name\": \"$name\",
       \"url\": \"$url\",
       \"description\": \"$description\",
       \"failed\": $1
     }
-  }" http://localhost:3000/api/update-item
+  }" https://dashtic.com/api/update-item
 }
+
 
 trap "report_failed true" ERR
 
-echo "TODO: build script goes here"
+fail
 
 report_failed false
+
